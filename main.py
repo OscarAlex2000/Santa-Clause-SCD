48169f5021d0fcc7c5c30ed9c1d0e59dc2fbda16
Uriel-Ramib <70905268+Uriel-Ramib@users.noreply.github.com>
2022-03-28 22:24:11 -0600

    Update main.py
    Se añadió la clase de los Duendes y los Regalos

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ from servivo import *
 Nombre_Renos = ["Rodolfo","Blitzen","Donder","Cupid","Comet","Vixen","Prancer","Dancer","Dasher"]
 Nombre_Duendes = ["Snowball","Bushy","Pepper"]
 Num_Renos = 0
+ren = 0
 Num_Duendes = 0
+Duendes = 0
 Despierta = 5
+Ayuda = 3
+
+Renos = 9
+AyudaDu = 3
 
 Sem_Santa = threading.Semaphore(0)
 Sem_Duendes = threading.Semaphore(3)
@@ -36,7 +42,7 @@ def santa():
 		for i in range(AyudaDu)
 	elif Num_Renos == Renos:
 		Num_Renos = 0
-		preparesleigh()
+		Prepararegalo()
 		for i in range(Renos):
 			Sem_Renos.release()    
 
@@ -52,20 +58,56 @@ def renos():
 	ren += 1
 	if ren == 9:
 		print("Los renos despiertan a Santa")
-	Sem_Santa.relesase()
+	Sem_Santa.release()
 	else:
 	print("Llegan los renos")
 	print("Los renos están listos")
 
-def main():
-    renos = [Reno("Rodolfo" + str(i)) for i in range(RENOS)]
-    duendes = [Duende("Pedro" + str(i)) for i in range(DUENDES)]
-    santa = Santa()
-    hiloSanta = Main(santa)
-    hiloSanta.run()
-    print("Que me ves?", renos[0].is_on_vacation, len(duendes), sep=", ")
-    print("---> Santa dice: Estoy cansado, dormiré un rato")
-    
+def duendes():
+	global ContDu
+	global elfs
 
+	num = ContDu
+	ContDu += 1
+	print("Los duendes dicen hola")
+
+	for i in range(Ayuda):
+		time.sleep(random.radiant(1,5))
+		Sem_Duendes.acquire()
+		elf = elfs + 1
+		elfs += 1
+		if elf <= 3:
+			print("Necesitamos ayuda!")
+			Sem_Santa.release()
+		Sem_Duendes.acquire()
+		print("Los duendes están siendo ayudados")
+
+def PreparaRegalos():
+	print("----> Santa carga los regalos")
+	print("----> Hasta la próxima navidad!!")
+	print("----> Santa duerme")
+	
+def main():
+    threads = []
+
+	sant = threading.Thread(target=santa)
+	threads.append(sant)
+
+	for i in range(Duendes):
+		duend = threading.Thread(target=duendes)
+		threads.append(duend)
+
+	for i in range(Renos):
+		duend = threading.Thread(target=duendes)
+		threads.append(Renos)
+
+	for t in threads:
+		t.start()
+
+	for t in threads:
+		t.join()
+
+	print("La navidad terminó")
+    
 if __name__ == '__main__':
-    main()
+    	main()
